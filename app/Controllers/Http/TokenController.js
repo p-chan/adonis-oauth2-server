6d244afe35ffba1b56f171c4d8a4d626981b683e
P-Chan <hello@0x50.io>
2018-05-04 07:57:30 +0900

    feat: Add refresh_token grant to OAuth 2.0

--- app/Controllers/Http/TokenController.js
+++ app/Controllers/Http/TokenController.js
@@ -6,19 +6,48 @@ class TokenController {
   async create ({ auth, request, response }) {
     try {
       const headers = request.headers()
-      const { username, password } = request.post()
+      const { grant_type, username, password, refresh_token } = request.post()
 
-      const token = await auth.withRefreshToken().attempt(username, password)
-      const claim = JSON.parse(
-        Buffer.from(token.token.split('.')[1], 'base64').toString()
-      )
+      switch (grant_type) {
+        case 'password':
+          response.send(await passwordGrant())
+          break
+        case 'refresh_token':
+          response.send(await refreshTokenGrant())
+          break
+        default:
+          throw new Error('not define grant_type')
+      }
 
-      return response.send({
-        access_token: token.token,
-        token_type: token.type,
-        expires_in: claim.exp - moment().unix(),
-        refresh_token: token.refreshToken
-      })
+      async function passwordGrant () {
+        const token = await auth.withRefreshToken().attempt(username, password)
+        const claim = JSON.parse(
+          Buffer.from(token.token.split('.')[1], 'base64').toString()
+        )
+
+        return {
+          access_token: token.token,
+          token_type: token.type,
+          expires_in: claim.exp - moment().unix(),
+          refresh_token: token.refreshToken
+        }
+      }
+
+      async function refreshTokenGrant () {
+        const token = await auth
+          .withRefreshToken()
+          .generateForRefreshToken(refresh_token)
+        const claim = JSON.parse(
+          Buffer.from(token.token.split('.')[1], 'base64').toString()
+        )
+
+        return {
+          access_token: token.token,
+          token_type: token.type,
+          expires_in: claim.exp - moment().unix(),
+          refresh_token: token.refreshToken
+        }
+      }
     } catch (e) {
       return response.send({
         error: 'fuck'
