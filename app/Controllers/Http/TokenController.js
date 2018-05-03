35635f357b1a83397254e8bb6240d024c2616686
P-Chan <hello@0x50.io>
2018-05-04 02:57:11 +0900

    refactor: Remove assertion flow from OAuth 2.0

--- app/Controllers/Http/TokenController.js
+++ app/Controllers/Http/TokenController.js
@@ -2,63 +2,17 @@
 
 class TokenController {
   async create ({ request, response }) {
-    const headers = request.headers()
-    const body = request.post()
+    try {
+      const headers = request.headers()
+      const body = request.post()
 
-    switch (body.grant_type) {
-      case 'password':
-        await password()
-        break
-      case 'assertion':
-        await assertion()
-        break
-      default:
-        return response.send({
-          error: 'not define grant_type'
-        })
-    }
-
-    async function password () {
       return response.send({
         type: 'password'
       })
-    }
-
-    async function assertion () {
-      let provider = ''
-
-      switch (body.provider) {
-        case 'twitter':
-          provider = await twitter()
-          break
-        case 'facebook':
-          provider = await facebook()
-          break
-        case 'google':
-          provider = await google()
-          break
-        default:
-          return response.send({
-            error: 'not define provider'
-          })
-      }
-
+    } catch (e) {
       return response.send({
-        type: 'assertion',
-        provider: provider
+        error: 'fuck'
       })
-    }
-
-    async function twitter () {
-      return 'twitter'
-    }
-
-    async function facebook () {
-      return 'facebook'
-    }
-
-    async function google () {
-      return 'google'
     }
   }
 }
